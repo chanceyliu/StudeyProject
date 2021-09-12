2fce0d9eee9747c9703fab226b933ca274aec312
chanceyliu <1445885764@qq.com>
2021-09-12 22:58:34 +0800

    feat: 新增第三题

---
+++ script/demo4Menu.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const filePath = path.resolve("./src/views/demo4");
+
+fs.readdir(filePath, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  let res = [];
+  data.forEach((item) => {
+    const file = fs.statSync(path.resolve(filePath, item));
+    if (file.isDirectory()) {
+      const fileArr = item.split("-");
+      console.log(fileArr, "-");
+      if (Number(fileArr[0]) > 0) {
+        res.push(`${fileArr[0]}. ${fileArr[2]}`);
+      }
+    }
+  });
+  fs.writeFile(
+    path.resolve(filePath, "./README.md"),
+    res.join("\n"),
+    "utf-8",
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("写入成功");
+    }
+  );
+});
