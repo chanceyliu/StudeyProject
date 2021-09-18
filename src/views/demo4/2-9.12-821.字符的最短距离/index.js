4361f4c8235c026774729a764d67b9a7ec0afc1a
chanceyliu <1445885764@qq.com>
2021-09-18 16:12:25 +0800

    chore: 文件夹名字加题号

---
+++ src/views/demo4/2-9.12-821.字符的最短距离/index.js
@@ -0,0 +1,13 @@
+function short(S, C) {
+  const data = S.split("");
+  const res = [];
+  for (let index = 0; index < data.length; index++) {
+    let flag = -1;
+    if (data[index] === C) {
+      flag = index;
+    }
+    if (flag === -1) {
+      res.push[index];
+    }
+  }
+}
