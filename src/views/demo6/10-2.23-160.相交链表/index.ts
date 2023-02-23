d35f76881f02d62a7d2b801868252172a5141e66
chanceyliu <1445885764@qq.com>
2023-02-23 14:49:20 +0800

    feat: 2.23

---
+++ src/views/demo6/10-2.23-160.相交链表/index.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null
+): ListNode | null {
+  const visited = new Set();
+  let temp = headA;
+  while (temp !== null) {
+    visited.add(temp);
+    temp = temp.next;
+  }
+  temp = headB;
+  while (temp !== null) {
+    if (visited.has(temp)) {
+      return temp;
+    }
+    temp = temp.next;
+  }
+  return null;
+}
--- src/views/demo6/README.md
+++ src/views/demo6/README.md
@@ -1,1 +1,7 @@
 2023-02-14 期算法训练
+
+![image-20230223144034742](https://chanceyliu-1301861058.cos.ap-chongqing.myqcloud.com/markdown/image-20230223144034742.png)
+
+![image-20230223144201446](https://chanceyliu-1301861058.cos.ap-chongqing.myqcloud.com/markdown/image-20230223144201446.png)
+
+![image-20230223144232518](https://chanceyliu-1301861058.cos.ap-chongqing.myqcloud.com/markdown/image-20230223144232518.png)
